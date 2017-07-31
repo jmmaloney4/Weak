0e0dcfdaa1308ca062c5fe3a42d165fbfec88115
Jack Maloney <jmmaloney4@gmail.com>
2017-07-30 22:49:54 -0500

    Initial Commit

---
+++ Sources/Weak.swift
@@ -0,0 +1,53 @@
+// Copyright © 2017 Jack Maloney. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Foundation
+
+public class Weak<T: AnyObject>: Equatable {
+    public weak var value: T?
+
+    public init() {
+        value = nil
+    }
+
+    public init(_ value: T) {
+        self.value = value
+    }
+
+    public var isNil: Bool { return value == nil }
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func == (lhs: Weak<T>, rhs: Weak<T>) -> Bool {
+        return lhs.value === rhs.value
+    }
+}
+
+extension Weak where T: Equatable {
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    public static func == (lhs: Weak<T>, rhs: Weak<T>) -> Bool {
+        return lhs.value == rhs.value
+    }
+}
+
+extension Array where Element: Weak<AnyObject> {
+    mutating func reap () {
+        self = self.filter({ !$0.isNil })
+    }
+}
